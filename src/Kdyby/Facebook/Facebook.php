b9c3b11e12d165c042f00bebabd7d31ae74ac9de
Daniel Robenek <daniel.robenek@me.com>
2014-05-07 14:04:54 +0200

    Facebook requires IRequest/IResponse instead of Request/Response

--- src/Kdyby/Facebook/Facebook.php
+++ src/Kdyby/Facebook/Facebook.php
@@ -54,12 +54,12 @@ class Facebook extends Nette\Object
 	protected $apiClient;
 
 	/**
-	 * @var \Nette\Http\Response
+	 * @var \Nette\Http\IResponse
 	 */
 	protected $httpResponse;
 
 	/**
-	 * @var \Nette\Http\Request
+	 * @var \Nette\Http\IRequest
 	 */
 	protected $httpRequest;
 
@@ -106,7 +106,7 @@ class Facebook extends Nette\Object
 	 */
 	public function __construct(
 		Configuration $config, SessionStorage $session, ApiClient $client,
-		Nette\Http\Request $httpRequest, Nette\Http\Response $httpResponse)
+		Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse)
 	{
 		$this->config = $config;
 		$this->httpResponse = $httpResponse;
@@ -567,7 +567,7 @@ class Facebook extends Nette\Object
 	 */
 	public function getCurrentUrl()
 	{
-		$url = clone $this->httpRequest->url;
+		$url = clone $this->httpRequest->getUrl();
 		if ($this->config->trustForwarded && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
 			$url->setHost($_SERVER['HTTP_X_FORWARDED_HOST']);
 		}
@@ -597,7 +597,7 @@ class Facebook extends Nette\Object
 
 		// Javascript sets a cookie that will be used in getSignedRequest that we need to clear if we can
 		$cookieName = $this->config->getSignedRequestCookieName();
-		if (array_key_exists($cookieName, $this->httpRequest->cookies)) {
+		if (array_key_exists($cookieName, $this->httpRequest->getCookies())) {
 			$this->httpResponse->deleteCookie($cookieName, '/', $this->getBaseDomain());
 			unset($_COOKIE[$cookieName]);
 		}
