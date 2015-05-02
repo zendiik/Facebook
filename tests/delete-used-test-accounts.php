e306f9bf612d9b5127d3481d8ea6bcf119913117
Filip Procházka <filip@prochazka.su>
2015-05-02 19:33:05 +0200

    Travis: delete old test accounts after build

---
+++ tests/delete-used-test-accounts.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$config = new Nette\Configurator();
+$config->setTempDirectory(__DIR__ . '/tmp');
+Kdyby\Facebook\DI\FacebookExtension::register($config);
+$config->addConfig(__DIR__ . '/KdybyTests/Facebook/files/config.kdyby.neon', $config::NONE);
+$config->addConfig(__DIR__ . '/KdybyTests/nette-reset.neon', !isset($config->defaultExtensions['nette']) ? 'v23' : 'v22');
+
+/** @var \Nette\DI\Container $container */
+$container = $config->createContainer();
+
+/** @var Kdyby\Facebook\Facebook $fb */
+$fb = $container->getByType('Kdyby\Facebook\Facebook');
+
+foreach ($userJsonFiles = glob(__DIR__ . '/tmp/test-user-*.json') as $userJsonFile) {
+	$users = json_decode(file_get_contents($userJsonFile));
+
+	foreach ($users as $user) {
+		echo "Deleting test user ", $user->id, " from ", basename($userJsonFile), " ... ";
+		try {
+			$result = $fb->api('/' . $user->id, 'DELETE', ['access_token' => $user->access_token]);
+			echo json_encode((array) $result), "\n";
+
+		} catch (\Kdyby\Facebook\FacebookApiException $e) {
+			echo $e->getMessage(), ": ", json_encode((array) $e->getResult()), "\n";
+		}
+	}
+}
+
+foreach ($userJsonFiles as $userJsonFile) {
+	@unlink($userJsonFile);
+}
