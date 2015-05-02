cd4bf5cd13461468b2c84bb2011bc12a04d3338d
Filip Procházka <filip@prochazka.su>
2015-05-02 20:20:44 +0200

    Refactored tests

--- tests/KdybyTests/bootstrap.php
+++ tests/KdybyTests/bootstrap.php
@@ -43,6 +43,3 @@ function id($val) {
 function run(Tester\TestCase $testCase) {
 	$testCase->run(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL);
 }
-
-
-require_once __DIR__ . '/Facebook/mock.php';
